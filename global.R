489ddc6cae6507a7f6a45f4d6903130e8ec47a7d
natalieoshea <ms.natalieoshea@gmail.com>
2022-02-03 13:52:14 -0500

    minimal working version

--- global.R
+++ global.R
@@ -1,2 +1,55 @@
 # load libraries
 library(shiny)
+library(tidyverse)
+
+# load data
+df <- read_csv("data/spotify.csv") %>%
+  mutate(`01/18/2022 08:14:43` = as.character(`01/18/2022 08:14:43`))
+
+# pivot data into long format and fix minor data issues
+df_long <- df %>%
+  pivot_longer(-c("id", "name"),
+               names_to = "date",
+               values_to = "popularity") %>%
+  # fix NA issue and set to numeric
+  mutate(popularity = ifelse(popularity == "n/a", NA, as.integer(popularity)),
+         # set date format using lubridate package (package::function syntax)
+         date = lubridate::mdy_hms(date),
+         # convert to short date format
+         date = lubridate::as_date(date)
+  ) %>%
+  # filter data to only include artists from summer 2021
+  filter(date >= "2021-05-31" & date <= "2021-09-06") %>%
+  arrange(date, desc(popularity))
+
+# save custom theme
+theme_custom <- function(...) {
+  # default minimal theme
+  theme_minimal() +
+    # set additional custom theme options
+    theme(
+      # remove legend
+      legend.position = "none",
+      # axis labels are somewhat unnecessary so let's remove them
+      axis.title = element_blank(),
+      # add some padding below title/subtitle
+      plot.title = element_text(margin = margin(0, 0, 5, 0), face = "bold"),
+      plot.subtitle = element_text(margin = margin(0, 0, 15, 0)),
+      plot.caption = element_text(margin = margin(15, 0, 0, 0)),
+      # align title and subtitle with plot margins
+      plot.title.position = "plot",
+      # set background color
+      plot.background = element_rect(fill = "grey90"),
+      panel.background = element_rect(fill = "grey90",
+                                      # make bounding box grey too
+                                      colour = "grey90"),
+      # change color of panel grid to white
+      panel.grid.major = element_line(size = 0.5, linetype = 'solid',
+                                      colour = "white"),
+      panel.grid.minor = element_line(size = 0.25, linetype = 'solid',
+                                      colour = "white"),
+      # set to mono font for fun and make dark grey
+      text = element_text(family = "mono", color = "grey20"),
+      ...
+    )
+}
--- server.R
+++ server.R
@@ -1,15 +1,34 @@
 # Define server logic required to draw a histogram
 function(input, output) {
 
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  final_df <- reactive({
+    req(input$artists)
 
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # filter data to only selected artists
+    final_df <- df_long %>%
+      filter(name %in% input$artists)
+
+    # set factor levels
+    # final_df$name <- factor(final_df$name, levels = input$artists)
+  })
+
+  output$plot <- renderPlot({
+    # aesthetics set in ggplot() are global and applied to the whole plot
+    ggplot(final_df(), aes(x = lubridate::date(date), y = popularity)) +
+      # aesthetics applied to a specific geom only apply to that layer
+      # slightly larger line under colored line gives impression of black outline
+      geom_line(color = "black", size = 2) +
+      geom_line(aes(color = name), size = 1.2) +
+      # set title
+      labs(title = "Change in artist popularity over Summer 2021",
+           subtitle = "Top 12 artists from Memorial Day to Labor Day",
+           caption = "Data: Spotify API") +
+      # set color palette (can also use scale_color_manual for more control)
+      scale_color_brewer(palette = "Paired") +
+      # set y axis to whole numbers
+      scale_y_continuous(breaks = scales::pretty_breaks()) +
+      theme_custom() +
+      # give each name it's own panel
+      facet_wrap(~name)
   })
 }
-
-# Run the application
-#shinyApp(ui = ui, server = server)
