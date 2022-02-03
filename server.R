ddb2ac0eb03c1e99d69824c0a01414c9f1c7e896
dangrous <5487802+dangrous@users.noreply.github.com>
2022-02-03 11:31:50 -0800

    add date range picker

--- server.R
+++ server.R
@@ -6,8 +6,38 @@ function(input, output) {
     x    <- faithful[, 2]
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
+<<<<<<< Updated upstream
     # draw the histogram with the specified number of bins
     hist(x, breaks = bins, col = 'darkgray', border = 'white')
+=======
+    # filter data to only selected artists and dates
+    final_df <- df_long %>%
+      filter(name %in% input$artists) %>%
+      filter(date >= format(input$daterange[1]) & date <= format(input$daterange[2]))
+
+    # set factor levels
+    # final_df$name <- factor(final_df$name, levels = input$artists)
+  })
+
+  output$plot <- renderPlot({
+    # aesthetics set in ggplot() are global and applied to the whole plot
+    ggplot(final_df(), aes(x = lubridate::date(date), y = popularity)) +
+      # aesthetics applied to a specific geom only apply to that layer
+      # slightly larger line under colored line gives impression of black outline
+      geom_line(color = "black", size = 2) +
+      geom_line(aes(color = name), size = 1.2) +
+      # set title
+      labs(title = "Change in artist popularity over Summer 2021",
+           subtitle = "Top 12 artists from Memorial Day to Labor Day",
+           caption = "Data: Spotify API") +
+      # set color palette (can also use scale_color_manual for more control)
+      scale_color_brewer(palette = "Paired") +
+      # set y axis to whole numbers
+      scale_y_continuous(breaks = scales::pretty_breaks()) +
+      theme_custom() +
+      # give each name it's own panel
+      facet_wrap(~name)
+>>>>>>> Stashed changes
   })
 }
 
--- ui.R
+++ ui.R
@@ -7,11 +7,21 @@ fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
+<<<<<<< Updated upstream
       sliderInput("bins",
                   "Number of bins:",
                   min = 1,
                   max = 50,
                   value = 30)
+=======
+      selectizeInput("artists", label = "Select Artists:",
+                  choices = unique(df$name), multiple = TRUE, options = list(maxItems = 12)),
+      dateRangeInput("daterange", "Date range: ",
+                     start = "2021-01-01",
+                     end   = "2021-12-31",
+                     min   = "2021-01-01",
+                     max   = "2021-12-31")
+>>>>>>> Stashed changes
     ),
 
     # Show a plot of the generated distribution
